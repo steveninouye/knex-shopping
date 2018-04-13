b872014fc536599f057609e90a952c33d92f7768
Steven Inouye <steveninouye@msn.com>
2018-04-13 07:32:41 -1000

    finished users routes

--- DS/users.js
+++ DS/users.js
@@ -7,9 +7,59 @@ const knex = require('knex')({
     database: 'knex_shopping'
   }
 });
-const GET_user_id = id => {
+const GET_users_id = id => {
   return knex('users')
     .where({ id })
     .select();
 };
-module.exports = { GET_user_id };
+
+const POST_users_login = email => {
+  return knex('users')
+    .where({ email })
+    .select();
+};
+
+const POST_users_register = email => {
+  return knex('users')
+    .where({ email })
+    .select();
+};
+
+const insertUser = (email, password) => {
+  return knex('users').insert([{ email, password }], '*');
+};
+
+const PUT_userID_forgot_password = id => {
+  return knex('users')
+    .where('id', id)
+    .select();
+};
+
+const updateUserPassword = (id, password) => {
+  return knex('users')
+    .where('id', id)
+    .update({ password }, '*');
+};
+
+const DELETE_userID = id => {
+  return knex('users')
+    .where('id', id)
+    .select();
+};
+
+const deleteUserID = id => {
+  return knex('users')
+    .where('id', id)
+    .del();
+};
+
+module.exports = {
+  GET_users_id,
+  POST_users_login,
+  POST_users_register,
+  insertUser,
+  PUT_userID_forgot_password,
+  updateUserPassword,
+  DELETE_userID,
+  deleteUserID
+};
--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,118 @@
 const router = require('express').Router();
-const { GET_user_id } = require('../DS/users');
+const {
+  GET_users_id,
+  POST_users_login,
+  POST_users_register,
+  insertUser,
+  PUT_userID_forgot_password,
+  updateUserPassword,
+  DELETE_userID,
+  deleteUserID
+} = require('../DS/users');
 
 router.get('/', (req, res) => {
   res.send('User Page');
 });
 
 router.get('/:user_id', (req, res) => {
-  GET_user_id(req.params.user_id)
+  GET_users_id(req.params.user_id)
     .then(user => {
+      //check if user exists
       const message = user[0] ? user[0] : { message: 'User not found' };
+      //send response
       res.send(message);
     })
+    //handle any errors connecting to database or acquiring data
     .catch(err => {
-      res.send('User not found');
+      res.send('Could Not Get Data ' + err);
     });
 });
 
-router.post('/users/login', (req, res) => {
-  GET_user_login(req.body.email, req.body.password)
-    .then(data => {})
-    .catch(err => {});
+router.post('/login', (req, res) => {
+  POST_users_login(req.body.email)
+    .then(user => {
+      //check if user exists
+      let message = user[0] ? user[0] : { message: 'User not found' };
+      //if user exists
+      if (user[0]) {
+        //check if password is correct
+        message =
+          user[0].password === req.body.password
+            ? user[0]
+            : { message: 'Incorrect password' };
+      }
+      //send response
+      res.send(message);
+    })
+    //handle any errors connecting to database or acquiring data
+    .catch(err => {
+      res.send('Could Not Get Data ' + err);
+    });
+});
+
+router.post('/register', (req, res) => {
+  POST_users_register(req.body.email)
+    .then(user => {
+      let message = user[0] ? { message: 'User already exists' } : user[0];
+      if (!user[0]) {
+        console.log(req.body.email);
+        console.log(req.body.password);
+        insertUser(req.body.email, req.body.password)
+          .then(user => {
+            res.send(user[0]);
+          })
+          .catch(err => {
+            res.send('Could Not Insert User ' + err);
+          });
+      } else {
+        res.send(message);
+      }
+    })
+    .catch(err => {
+      res.send('Could Not Get Data ' + err);
+    });
+});
+
+router.put('/:user_id/forgot-password', (req, res) => {
+  PUT_userID_forgot_password(req.params.user_id)
+    .then(user => {
+      if (user[0]) {
+        updateUserPassword(req.params.user_id, req.body.password)
+          .then(user => {
+            res.send({ message: 'New password created!' });
+          })
+          .catch(err => {
+            res.send(err);
+          });
+      } else {
+        res.send('Could Not Find User');
+      }
+    })
+    .catch(err => {
+      res.send('Could Not Get Data ' + err);
+    });
+});
+
+router.delete('/:user_id', (req, res) => {
+  DELETE_userID(req.params.user_id)
+    .then(user => {
+      if (user[0]) {
+        deleteUserID(req.params.user_id)
+          .then(data => {
+            res.send({
+              message: `User id: ${req.params.user_id} successfully deleted`
+            });
+          })
+          .catch(err => {
+            res.send(err);
+          });
+      } else {
+        res.send({ message: 'User ID not found' });
+      }
+    })
+    .catch(err => {
+      res.send(err);
+    });
 });
 
 module.exports = router;
